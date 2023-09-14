e62eb7df445b2000c0ac47c60b5d10eba8d20e95
Dark-Fahim <itsmunna1066@gmail.com>
2023-09-14 08:10:23 +0600

    first commit

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './Blogs.css'
+import { useEffect } from 'react';
+
+const Blogs = () => {
+
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
